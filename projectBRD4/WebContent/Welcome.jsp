aa0ed218784176e2f89af8e29bbe7e2ad7bbca10
Ganesharun96 <44750297+Ganesharun96@users.noreply.github.com>
2018-12-01 16:25:20 +0530

    alteration
    alteration

--- projectBRD4/WebContent/Welcome.jsp
+++ projectBRD4/WebContent/Welcome.jsp
@@ -4,9 +4,10 @@
 <html>
 <head>
 <meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<title>Insert title here</title><h1>Welcome<h1></h1>
 </head>
 <body>
-welcome....
+<a href="newuser.html">New user<a><br><br>
+<a href="file.html">File<a><br><br>
 </body>
 </html>
--- projectBRD4/src/com/demo/login.java
+++ projectBRD4/src/com/demo/login.java
@@ -1,6 +1,5 @@
 package com.demo;
 
-
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
@@ -15,44 +14,53 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.org.apache.xpath.internal.operations.And;
+
+import sun.security.action.GetIntegerAction;
+
 /**
  * Servlet implementation class WelcomeServlet
  */
 @WebServlet("/login")
 public class login extends HttpServlet {
+
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String name = request.getParameter("name");
 		String password = request.getParameter("password");
-		if(validateLogin(name, password) )
-		{
-		  response.sendRedirect("file.html");
-			//response.sendRedirect("Welcome.jsp");
-		}
-		else
-		{
-			response.sendRedirect("LoginError.jsp");
+		String makerorchecker = request.getParameter("makerorchecker");
+		if (validateLogin(name, password, makerorchecker)) {
+			if (makerorchecker.equals("maker")) {
+//				response.sendRedirect("file.html");
+				 response.sendRedirect("Welcome.jsp");
+			}
+//			 response.sendRedirect("Welcome.jsp");
+		} else {//response.sendRedirect("Welcome.jsp");
+			response.sendRedirect("loginpage.html");
 		}
 //		PrintWriter out =response.getWriter();
 //		out.println(name);
 //		response.sendRedirect("file.html");
 	}
 
-	private boolean validateLogin(String name, String password) {
+	private boolean validateLogin(String name, String password, String makerorchecker) {
 		try {
 //		String	names=name;
 //		String	passwords=password;
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe", "hr", "hr");
 			Statement statement = connection.createStatement();
-			String query = "select * from dbconnection where name ='" + name +"' and password ='"+ password +"'";
-		
+			String query = "select * from dbconnections where name ='" + name + "' and password ='" + password
+					+ "'and makerorchecker='" + makerorchecker + "'";
+
 			ResultSet Result = statement.executeQuery(query);
 			System.out.println("Valid Login!");
-		while(Result.next()) {
+			
+			if(Result.next()) {
 //	if (Result.next()) {
-//			if(Result.getInt(1) > 0)
+//			if(Result.getInt(1) > 0)`
+				//System.out.println(toString());
 				return true;
 			}
 
